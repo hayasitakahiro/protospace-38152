25c7e5739695bdea99780d31ab26469d63b7f1a9
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-07-11 10:38:33 +0900

    :destroy追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
- before_action :authenticate_user!,  only: [:show, :edit]
+ before_action :authenticate_user!,  only: [:show, :edit, :destroy]
  
 
   def index
